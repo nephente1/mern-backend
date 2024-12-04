a4feb500b8314cfefe70c504c6181d4b3d3affdd
Kasia <katarzynapol@vp.pl>
2024-12-04 11:30:46 +0100

    change access-control clear

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json())
 
 //"Content-Security-Policy", "default-src 'self';
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "script-src 'self'; style-src 'unsafe-inline'; img-src 'self'; font-src 'self' https://fonts.googleapis.com;");
+  res.setHeader("Content-Security-Policy", "script-src 'self'; style-src 'unsafe-inline'; img-src 'self' https:; font-src 'self' https://fonts.googleapis.com;");
   res.setHeader('Access-Control-Allow-Origin', '*'); // Zezwól na połączenia z Twojej aplikacji frontendowej
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Zezwól na określone metody HTTP
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); // Zezwól na określone metody HTTP
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Zezwól na określone nagłówki
   if (req.method === 'OPTIONS') {
     return res.status(200).end(); // Zakończ zapytanie preflight z kodem 200
