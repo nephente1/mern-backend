3d3236d5f2bf68a5c4ae5a55bbc6befcb6aa03f2
Kasia <katarzynapol@vp.pl>
2024-12-04 11:15:37 +0100

    change access-control handle OPTIONS

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ app.use(express.json())
 //"Content-Security-Policy", "default-src 'self';
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "script-src 'self'; style-src 'unsafe-inline'; img-src 'self'; font-src 'self' https://fonts.googleapis.com;");
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Zezwól na połączenia z Twojej aplikacji frontendowej
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Zezwól na połączenia z Twojej aplikacji frontendowej
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Zezwól na określone metody HTTP
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Zezwól na określone nagłówki
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Zakończ zapytanie preflight z kodem 200
+  }
     console.log(req.path, req.method)
     next()
 })
